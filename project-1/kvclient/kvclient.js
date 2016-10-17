4d660c722ebe8b41ad4aecb87effd1e518e4258c
Waqid <wmv214@nyu.edu>
2016-10-17 14:20:19 -0400

    Write errors to stderr instead of terminal

--- project-1/kvclient/kvclient.js
+++ project-1/kvclient/kvclient.js
@@ -12,9 +12,7 @@ var my_value = null;
 
 //Check if its a valid command
 if(process.argv.length<6 || process.argv.length>7){
-  console.log('\n');
-  console.error('Incorrect command. It should be: \nnode kvclient -server [host:port] [subcmd] [my_key] <my_value>');
-  console.log('\n');
+  new Error('Incorrect command. It should be: \nnode kvclient -server [host:port] [subcmd] [my_key] <my_value>');
   process.exit(2);
 }else {
   process.argv.forEach(function(val, index, array) {
@@ -42,45 +40,50 @@ var client = thrift.createClient(KVStorage, connection);
 
 if(action=='-get'){ //If GET command
   client.kvget(my_key, function(err, result) {
-    if (err || result.error == ttypes.ErrorCode.kError) {
-      console.log('\n');
-      console.error(('There was an error on the server.'));
-      console.log('\n');
+    if (err){
+      new Error(err);
       process.exit(2);
-    }else if(result.error == ttypes.ErrorCode.kSuccess){
+    } else if(result.error == ttypes.ErrorCode.kError) {
+        new Error(result.errortext);
+        process.exit(2);
+    } else if(result.error == ttypes.ErrorCode.kSuccess){
       console.log('\n');
       console.log('Value: ',result.value);
       console.log('\n');
       process.exit(0);
-    }else{
-      console.log('\n');
-      console.error((result.errortext));
-      console.log('\n');
+    } else {
+      new Error(result.errortext);
       process.exit(1);
     }
   });
 } else if(action=='-set'){  //If SET command
     client.kvset(my_key, my_value, function(err, result) {
-      if (err || result.error == ttypes.ErrorCode.kError) {
+      if (err){
+        new Error(err);
         process.exit(2);
+      } else if(result.error == ttypes.ErrorCode.kError) {
+          new Error(result.errortext);
+          process.exit(2);
       } else {
-        process.exit(0);
+          process.exit(0);
       }
     });
 } else if(action=='-del'){  //If DEL command
   client.kvdelete(my_key, function(err, result) {
-    if (err || result.error == ttypes.ErrorCode.kError) {
+    if (err){
+      new Error(err);
       process.exit(2);
+    } else if(result.error == ttypes.ErrorCode.kError) {
+        new Error(result.errortext);
+        process.exit(2);
     } else {
-      process.exit(0);
+        process.exit(0);
     }
   });
 }
 
 //If an uncaught exception occured such as if the server is not available.
 process.on('uncaughtException', function (err) {
-    console.log('\n');
-    console.error((err.errno));
-    console.log('\n');
+    new Error(err);
     process.exit(2)
 });
